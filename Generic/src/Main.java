ccab4d648110dafce02fc2f0928a6d5108a2795d
Belfegorrr <pelichek.iago@gmail.com>
2024-03-04 16:52:31 -0300

    feat: add class abstrata

--- Generic/src/Main.java
+++ Generic/src/Main.java
@@ -1,12 +1,11 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {        
         ArrayList<Carro> lista = new ArrayList<Carro>();
         
         lista.add(new Troller());
-        lista.add(new Volkswagen());            
+        lista.add(new Volkswagen());    
 
         System.out.println(lista.get(0));
         System.out.println(lista.get(1));
